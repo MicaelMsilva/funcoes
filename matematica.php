b77f663e9cdf2acec18aba300d2e5cde3cae0ea6
MicaelMsilva <130592248+MicaelMsilva@users.noreply.github.com>
2023-11-22 17:48:07 -0300

    Update matematica.php

--- matematica.php
+++ matematica.php
@@ -1,1 +1,25 @@
+<?php
+//$nome = readline("Qual o pokemon:")
+$short = "p";
+$long = [
+	"pokemon:"
+];
 
+$options = getopt($short, $long);
+print_r($options);
+
+$nome = $options['pokemon'];
+
+$dados_txt = file_get_contents("https://pokeapi.co/api/v2/pokemon/{$nome}");
+
+$pokemon = json_decode($dados_txt, true);
+print (strtoupper ($pokemon['name']) ."\n");
+print ("altura:" . $pokemon['height']. "\n");
+print ("peso:" . $pokemon['weight']. "\n");
+
+print("movimentos: \n"); 
+ foreach($pokemon['moves'] as $move)
+ {
+print (" - " . $move ['move']['name']. "\n");
+
+}
